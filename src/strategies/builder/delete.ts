417741061fe7d9610c85f044eab0ed6930eaae08
Jorge Junior <jorge.junior.568@live.com>
2020-09-10 10:02:37 -0300

    feat: delete strategy

--- src/strategies/builder/delete.ts
+++ src/strategies/builder/delete.ts
@@ -1,3 +1,23 @@
-import { ITable } from '../../protocols'
+import { ITable, IWhere } from '../../protocols'
 
-export async function DeleteStrategy(table: ITable): Promise<void> {}
+export async function DeleteStrategy(table: ITable): Promise<void> {
+  const idCondition = table._where.filter(
+    (condition: IWhere) => condition.column.toLowerCase() === 'id'
+  )[0]
+  if (!idCondition)
+    throw new Error('Id on where condition was not specified at DeleteStrategy')
+
+  await table
+    .client()
+    .request(
+      'SP',
+      'POST',
+      `/_api/Web/Lists/GetByTitle('${table._table}')/Items(${idCondition.value})/recycle()`,
+      {
+        contentType: 'application/json;odata=verbose',
+        accept: 'application/json;odata=verbose',
+        'IF-MATCH': '*',
+        'X-HTTP-Method': 'DELETE',
+      }
+    )
+}
